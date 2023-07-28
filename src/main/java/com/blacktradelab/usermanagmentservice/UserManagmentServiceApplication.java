24275ed70174d99bfdccc895c1a296af107f03cf
Edgar Doutón <38222402+lEdgaar@users.noreply.github.com>
2023-07-28 09:17:50 +0200

    Add files via upload

---
+++ src/main/java/com/blacktradelab/usermanagmentservice/UserManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blacktradelab.usermanagmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blacktradelab/usermanagmentservice/UserManagmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blacktradelab.usermanagmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
